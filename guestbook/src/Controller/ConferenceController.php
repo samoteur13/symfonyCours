3cb7c22c8f927c77f8ca8baed7d6fc5592ee8991
samy chabani <s.chabani13@gmail.com>
2023-02-21 15:21:45 +0000

    save

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -2,23 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index( ConferenceRepository $conferenceRepository): Response
     {
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
+        return new Response($this->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+                ]
+            )
+        );
 
-        return new Response(<<<EOF
-            <html>
-                <body style="display:flex;justify-content:center" ><img width="500px" height="500Px" src="/images/under-construction.gif" /></body>
-            </html>
-        EOF);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+        public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+        {
+            $offset = max(0, $request->query->getInt('offset', 0));
+            $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+            return new Response($this->render('conference/show.html.twig', [
+                'conference' => $conference,
+                 'comments' => $paginator,
+                 'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                 'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            ]));
     }
 }
