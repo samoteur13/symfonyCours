109f55578612cf4c4b69623e21aa966da6304589
samy chabani <s.chabani13@gmail.com>
2023-02-22 10:22:38 +0000

    save ajout sur conférence et comment de nouveaux champs

--- guestbook/src/Controller/Admin/ConferenceCrudController.php
+++ guestbook/src/Controller/Admin/ConferenceCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class ConferenceCrudController extends AbstractCrudController
 {
@@ -12,14 +17,16 @@ class ConferenceCrudController extends AbstractCrudController
         return Conference::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('city'),
+            TextField::new('year'),
+            TextField::new('slug')->hideOnForm(),
+            BooleanField::new('isInternational'),
         ];
     }
-    */
+    
 }
